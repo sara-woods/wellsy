78dfadd356cc42ea703fc2341a25f83404f01080
Erickson P <joelerickson@hotmail.co.uk>
2021-03-02 15:06:32 +0000

    added the modal

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,7 @@
 class SessionsController < ApplicationController
-  def new
-    respond_to do |format|
-      format.html
-      format.js
-    end
-  end
 
   def index
-    @activities = Activity.all
-  end
-
-  def show
-    @activities = Activity.find(params[:id])
+    @sessions = Session.all
   end
 
 end
