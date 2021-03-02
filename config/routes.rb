4e9b8f0bb81fae2218cfa0d7dddc4ad4fa4c6f2d
Erickson P <joelerickson@hotmail.co.uk>
2021-03-02 14:04:05 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :activities do
-    resources :sessions, only: [:new]
-  end
+  resources :activities
+  resources :sessions
 end
