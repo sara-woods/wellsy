c3894edfc88ba5a199683afbb697d82dd150eceb
Erickson P <joelerickson@hotmail.co.uk>
2021-03-02 12:57:47 +0000

    added line to controller, created new.js.erb, added to index and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+  def new
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+    @activities = Activity.find(params[:id])
+  end
+
 end
