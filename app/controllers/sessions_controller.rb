b14e04a6767de60cde55de174567c355a889949f
Sara Lotfi <lotfi.sara@gmail.com>
2021-03-03 13:17:56 +0100

    index only future sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,13 @@ class SessionsController < ApplicationController
 
 
   def my_bookings 
-    @my_upcoming_bookings = current_user.bookings.joins(:session).where("sessions.end_time >= ?", DateTime.now).order("sessions.start_time asc")
-    @my_past_bookings = current_user.bookings.joins(:session).where("sessions.end_time < ?", DateTime.now).order("sessions.start_time desc")
+    @my_upcoming_bookings = current_user.bookings.joins(:session).where("sessions.end_time >= ?", DateTime.now).order(start_time: :asc)
+    @my_past_bookings = current_user.bookings.joins(:session).where("sessions.end_time < ?", DateTime.now).order(start_time: :desc)
   end
 
 
   def index
-    @sessions = Session.all.order(start_time: :asc)
+    @sessions = Session.where("sessions.end_time >= ?", DateTime.now).order(start_time: :asc)
   end
 
 
