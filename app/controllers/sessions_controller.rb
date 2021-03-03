b62a3de2531d5f257762bda234743eeee3090e15
Luke Billings <lukecbillings@gmail.com>
2021-03-03 16:58:14 +0000

    past bookings working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
-class SessionsController < ApplicationController
+B@class SessionsController < ApplicationController
 
 
-  def my_bookings 
+  def my_bookings
     @my_upcoming_bookings = current_user.bookings.joins(:session).where("sessions.end_time >= ?", DateTime.now).order(start_time: :asc)
     @my_past_bookings = current_user.bookings.joins(:session).where("sessions.end_time < ?", DateTime.now).order(start_time: :desc)
   end
