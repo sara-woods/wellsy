e47b9abee9581f47ac938b70ff7eddceea4ce68e
Luke Billings <lukecbillings@gmail.com>
2021-03-04 16:39:54 +0000

    can access attendees with .attendees

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,7 @@
 class Session < ApplicationRecord
   belongs_to :activity
   has_many :bookings
+  has_many :attendees, through: :bookings, source: :user
 
 
   validates :start_time, presence: true
@@ -15,7 +16,7 @@ class Session < ApplicationRecord
 
   def class_size
     if min_participants > max_participants
-      errors.add(:min_participants, "cannot exceed # of max participants") 
+      errors.add(:min_participants, "cannot exceed # of max participants")
     end
   end
 
