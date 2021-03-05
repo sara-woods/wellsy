c4f352cd6f9e17d934ffa0c0ca5a91cc6df3790c
Gabriel Hardy-Francon <gabrielhardyfrancon@gmail.com>
2021-03-05 17:03:29 +0100

    adjusting categories pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
 Booking.destroy_all
 Session.destroy_all
 Activity.destroy_all
@@ -37,6 +38,17 @@ end
 #categories
 category_array = ["Endurance", "Yoga", "Strength Training", "Aerobics", "Bands", "Body Hit", "Pilates", "Shadow Boxing", "Core Workout"]
 
+categories = {
+  "Endurance": "https://images.unsplash.com/photo-1434596922112-19c563067271?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  "Yoga": "https://images.unsplash.com/photo-1552196563-55cd4e45efb3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1226&q=80",
+  "Strength Training": "https://images.unsplash.com/photo-1533560777802-046814bc297c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+  "Aerobics": "https://images.unsplash.com/photo-1533547252597-ef7bf6c4479f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  "Bands": "https://images.unsplash.com/photo-1517637382994-f02da38c6728?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+  "Body Hit": "https://images.unsplash.com/photo-1544717684-1243da23b545?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  "Pilates": "https://images.unsplash.com/photo-1518611012118-696072aa579a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" ,
+  "Shadow Boxing": "https://images.unsplash.com/photo-1593352216894-89108a0d2653?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  "Core Workout": "https://images.unsplash.com/photo-1516208398649-d5d401ba8c49?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"}
+
 # 5.times do
 #   category = Category.create!(name: category_array.sample)
 # end
@@ -68,8 +80,15 @@ description_array = [
 
 
 # categories new
-category_array.each do |category|
-  category = Category.create!(name: category)
+# category_array.each do |category|
+#   category = Category.create!(name: category)
+# end
+
+categories.each do |category, url|
+  file = URI.open(url)
+  category = Category.new(name: category)
+  category.photo.attach(io: file, filename: 'nes.png', content_type: 'image/jpeg')
+  category.save
 end
 
 
