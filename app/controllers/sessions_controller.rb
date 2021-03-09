9f140492606e43a962ebb7f247bc2fcf0b7fda03
Luke Billings <lukecbillings@gmail.com>
2021-03-09 12:29:40 +0000

    dropdown search removed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,7 @@ class SessionsController < ApplicationController
   def index
     # Remember to change this back to only future ssessions showing on index instead of all
     # @sessions = Session.where("sessions.end_time >= ?", DateTime.now).order(start_time: :asc)
+    @categories = Category.all
     @sessions = Session.all
     if params[:session] && params[:session] != ""
       activities = Activity.where("name ILIKE :query OR description ILIKE :query", query: "%#{params[:session]}%")
