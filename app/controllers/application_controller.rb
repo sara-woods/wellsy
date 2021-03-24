2e10eecce352d5d6d7acea48bd1210ef6d1d1bbd
Sara Lotfi <lotfi.sara@gmail.com>
2021-03-24 17:37:18 +0100

    redirecting user after login to the page they were just at

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location? 
 
   def configure_permitted_parameters
 
@@ -13,5 +14,19 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  # Redirect user after signin to the page they were just at
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
   end
 end
